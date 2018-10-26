d744b4e65e468e2ee9e6c2a1f664e70680777fae
omk725 <omk725@gmail.com>
2018-10-25 17:16:06 -0700

    complete first method

--- _.js
+++ _.js
@@ -1,6 +1,16 @@
+const _ = {};
 
+_.clamp = function (number, lower, upper) {
+  if (number < lower) {
+    return lower;
+  } else if (number > upper) {
+    return upper;
+  }else {
+    return number;
+  }
+};
 
-
+console.log(_.clamp(1,3,4));
 
 
 // Do not write or modify code below this line.
